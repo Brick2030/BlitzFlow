9e94d685ccb225aa661fe5b2709c3c407bcb4ce3
brick <patchouliknowledge1998@proton.me>
2024-04-14 00:25:09 +0700

    Major changes. Timercontrol module was remade

--- timercontrol.py
+++ timercontrol.py
@@ -1,11 +1,10 @@
 import os
 import pickle
 import time
-import tasks
 import widgets
 import cycles
 import datetime
-
+from prettytable import PrettyTable
 
 # Обновить ввод циттат. Разнообразить их.
 # Попробывать форматировать вывод в таблицу. Посмотреть библиотеки.
@@ -18,124 +17,153 @@ import datetime
 
 # Таблица как объект будет сохраняться загружаться удаляться, отобраться и т.д.
 
+# Раздробить цельный список обхектов в несколько списков по статусам.
 
-taskList = []
-
-def add(ccycles, name, status, result):
-    task1 = tasks.task(ccycles, name, status, result)
-    taskList.append(task1)
-
-def remove(index):
-    taskList.pop(index)
-
-def complete(index, time):
-    taskList[index].ChangeStatus(3)
-    taskList[index].ChangeResult(time)
-
-def fail(index):
-    taskList[index].ChangeStatus(0)
-
-def move(index, index2):
-    tempObj = taskList[index2]
-    taskList[index2] = taskList[index]
-    taskList[index] = tempObj
 
 # Naming files for saving.
 current_date = datetime.date.today()
-TodayFileName = current_date.strftime("%d-%m-%Y") + ".task  "
-#print(TodayFileName)
+TodayFileName = current_date.strftime("%d-%m-%Y") + ".task"
 
+# Main Class for task object
+# 0 - Ongoing.
+# 1 - Finished
+# -1 - Failed (delayed).
+# Object will store only number status. And table itself will translate it words
+# Or maybe it's better to use dictionary. But why to complicate such simple things.
+class TaskClass:
+    def __init__(self, cycles, name, status, result):
+        self.cycles = cycles
+        self.name = name
+        self.result = result 
+
+
+UnfinishedLIST = []
+FinishedLIST = []
+FailedLIST = []
+GlobalLIST = [UnfinishedLIST, FinishedLIST, FailedLIST] # ARRAY OF ARRAYS. ONGOING, FINISHED, FAILED
+
+
+#taskList = [] # MAIN ARRAY OF TASKS
+quote = widgets.info() # Object for quote.
+
+
+
+
+
+
+
+ShowUnfinished = True
+ShowFinished = True
+ShowFailed = True
+
+
+
+##############################[Controls]#################################################
 
 def save(): # goals are array
     with open(TodayFileName, "wb") as f:
-        pickle.dump(taskList, f)
+        pickle.dump(GlobalLIST, f)
 
 def load(filename):
     with open(filename, "rb") as f:
         loaded_goals = pickle.load(f)
     return loaded_goals # return array
 
+def showList(arrayName):
+    TaskTab = PrettyTable()
+    TaskTab.clear()
+    TaskTab = PrettyTable(['Index','Cycles', 'Name', 'Result'])
+    TaskTab.align = "r"
 
-quote = widgets.info() # Object for quote.
+    for x, i in enumerate(arrayName):
+        TaskTab.add_row([x, i.cycles, i.name, i.result])
+    
+    print(TaskTab)
+
+def add(cycles, name):
+    add_task = TaskClass(cycles, name, "0", "0")
+    UnfinishedLIST.append(add_task)
+
+def fail(index):
+    temp_task = UnfinishedLIST[index]
+    FailedLIST.append(temp_task)
+    UnfinishedLIST.pop(index)
+
+def finish(index, cycles):
+    temp_task = UnfinishedLIST[index]
+    temp_task.result = cycles
+    FinishedLIST.append(temp_task)
+    UnfinishedLIST.pop(index)
 
 
-# Раздробить цельный список обхектов в несколько списков по статусам.
 
 
-
-
+##############################[Main Core]#################################################
 
 
 while(True):
     os.system('cls' if os.name == 'nt' else 'clear')
-
     print(quote.rand_quote()) # Random Quote.
-
     print("Cycles until sleep:", cycles.returnCycles(), "\n")
-    print("%7s %10s %40s %10s %10s %20s" % ("[Index]", "[Cycles]", "[Name]", "[Status]", "[Result]", "[Result 2]")) # Header format.
-
-
-    cycleSumm = 0 # Summ of cycles.
-    for x, i in enumerate(taskList): # This loop outputs tasks that need to be solved rn.
-        array = i.ReturnTask() # Array = one task. It's array bc it returns array.
-
-
-        if (array[2] == "Nothing" or array[2] == "Ongoing"):
-            cycleSumm = cycleSumm + int(array[0])
-            print("%7s %10s %40s %10s %10s %20s" % (str(x), str(array[0]), array[1], array[2], str(array[3]), "NO RESULT"))
-   
-    print("%10s" % ("Summ: "), cycleSumm)
-
     
-    for x, i in enumerate(taskList): # Second table for completed tasks
-        array = i.ReturnTask() # array = task. 
-        if(array[2] == "Completed"):
-            print("%7s %10s %40s %10s %10s %20s" % (str(x), str(array[0]), array[1], array[2], str(array[3]), str(float(array[3])/float(array[0])*100)), "%")
+    if(ShowUnfinished):
+        print("ONGOING")
+        showList(UnfinishedLIST)
+    if(ShowFinished):
+        print("FINISHED")
+        showList(FinishedLIST)
+    if(ShowFailed):
+        print("FAILED")
+        showList(FailedLIST)
 
+
+
+
+
+##############################[Commands]########(Included in while)#######################
 
     com = input() # Command input
     
     match com:
-        case "add":
-            TaskCycles = input("cycles: ")
-            TaskName = input("name: ")
-            add(TaskCycles, TaskName, 1, 0)
-
-        case "finish":
-            index = input("Index to complete: ")
-            time = input("How many cycles used to finish: ")
-            complete(int(index), (time))
-
-        case "fail":
-            index = input("Index to fail: ")
-            fail(int(index))
-
-        case "remove":
-            index = input("Index to remove")
-            remove(int(index))
-
-        case "exit":
-            quit()
-
-        case "move":
-            index = input("Enter index of task to move")
-            index2 = input("Enter index where to move")
-            move(int(index), int(index2))
-
-        case "show delayed":
-            print("Delayed tasks")
-            # Сделать булеан переменную на отображения виджета отложенной задачи.
-            # Стоит ли делать отложенную задачу подклассом обычной задачи?
-
         case "save":
             save()
 
         case "load":
             inp = input("Enter file name: dd-mm-yy without .task extension: ")
-            taskList = load(inp + ".task")
+            #GlobalLIST = load(inp)
+            GlobalLIST = load(inp + ".task")
+
+            UnfinishedLIST = GlobalLIST[0]
+            FinishedLIST = GlobalLIST[1]
+            FailedLIST = GlobalLIST[2]
+            #GlobalLIST = [UnfinishedLIST, FinishedLIST, FailedLIST] # ARRAY OF ARRAYS. ONGOING, FINISHED, FAILED
+
+        case "add":
+            temp_c = input("Cycles to finish: ")
+            temp_n = input("Task name:")
+            add(temp_c, temp_n)
+
+        case "fail":
+            temp_index = input("Index to fail: ")
+            fail(int(temp_index))
+
+        case "finish":
+            temp_index = input("Index to finish: ")
+            temp_efficiency = input("Cycles used: ")
+
+            finish(int(temp_index), int(temp_efficiency))
+
+        case "remove":
+            temp_index = input("Index to remove: ")
+            UnfinishedLIST.pop(int(temp_index))
             
+        case "less":
+            ShowFailed = False
+            ShowFinished = False
+
+        case "more":
+            ShowFailed = True
+            ShowFinished = True
 
 
-        case "help":
-            print("\nadd\nfinish\nfail\nremove\nhelp\nexit\nmove\n")
-            i = input("type anything to close help: ")
+
