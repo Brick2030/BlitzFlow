0ceec9328ec9ea2cba77e90e6a7ec2178bae13d2
brick <patchouliknowledge1998@proton.me>
2024-05-15 00:08:36 +0700

    few changes

--- newfeature.py
+++ newfeature.py
@@ -4,10 +4,17 @@ import math
 
 def NumberOfWeek():
     Month = settings.YearStartMonth
+
+    some_date = datetime(2022, Month, 1)
+    now_date = datetime(2023, 9, 1)
     
-    some_date = datetime(2023, Month, 1)
-    now_date = datetime.now()
+    #now_date = datetime.now()
     delta = (now_date - some_date).days+1
+    delta2 = (now_date - testdate).days+1
+    print("delta2", delta2)
+    year_delta = (now_date.year - some_date.year)
+    print("year delta", year_delta)
+
     firstday = some_date.isocalendar().weekday
     weeknum = 1
 
